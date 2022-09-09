667077bba6507e7c6de9f494bff0faee832a7f09
John Marshall <john.marshall@canonical.com>
2022-09-09 14:52:35 -0600

    Updated hpctlib

--- src/hpctlib/interface/value.py
+++ src/hpctlib/interface/value.py
@@ -8,52 +8,67 @@
 Note: Test exposing values rather than codecs. Codecs would be hidden.
 """
 
-from .base import Value
+import ipaddress
+
+from .base import NoValue, Value
 from . import codec
 
 
-class AutoCodecValue(Value):
-    def __init__(self, default, checker=None):
-        clsname = self.__class__.__name__
-        codeccls = getattr(codec, clsname)
-        super().__init__(codeccls(), default, checker))
+class XValue(Value):
+    codec = None
+    default = None
+    checker = None
+
+    def __init__(self, default=NoValue, checker=None):
+        self.default = self.default if default == NoValue else default
+        self.checker = self.checker if checker == None else checker
+        super().__init__(self.codec, self.default, checker)
 
 
-class Boolean(AutoCodecValue):
+class Boolean(XValue):
+    codec = codec.Boolean()
+    default = False
+
+
+class Blob(XValue):
+    codec = codec.Blob()
+    default = None
+
+
+class Dict(XValue):
+    codec = codec.Dict()
+    default = None
+
+
+class Float(XValue):
+    codec = codec.Float()
+    default = 0.0
+
+
+class Integer(XValue):
+    codec = codec.Integer()
+    default = 0
+
+
+class IPAddress(XValue):
+    codec = codec.IPAddress()
+    default = ipaddress.IPv4Address("0.0.0.0")
+
+
+class IPNetwork(XValue):
+    codec = codec.IPNetwork()
+    default = ipaddress.IPv4Network("0.0.0.0")
+
+
+class Noop(XValue):
+    codec = codec.Noop()
+    default = ""
+
+
+class Ready(Boolean):
     pass
 
 
-class Blob(AutoCodecValue):
-    pass
-
-
-class Dict(AutoCodecValue):
-    pass
-
-
-class Float(AutoCodecValue):
-    pass
-
-
-class Integer(AutoCodecValue):
-    pass
-
-
-class IPAddress(AutoCodecValue):
-    pass
-
-
-class IPNetwork(AutoCodecValue):
-    pass
-
-
-class Noop(AutoCodecValue):
-    pass
-
-
-class Ready(AutoCodecValue):
-    pass
-
-
-class String(AutoCodecValue):
-    pass
+class String(XValue):
+    codec = codec.String()
+    default = ""
